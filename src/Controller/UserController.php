d8f304ddc69abebc4e81ffb6d964c63339a0bca6
lkurtz <laura.kurtz@mewo-campus.fr>
2024-10-04 10:53:48 +0200

    searchbar made into a component some changes on buttons display if app.user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_user_profile')]
+    public function index(): Response
+    {
+        return $this->render('user/profile.html.twig', [
+	        'user' => $this->getUser(),
+        ]);
+    }
+}
