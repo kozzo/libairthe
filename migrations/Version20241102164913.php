2e40172814b0d96859aee5b0bdd1b54daac50e6b
Laura KURTZ <laura.kurtz@mewo-campus.fr>
2024-11-02 18:30:25 +0100

    entity update

---
+++ migrations/Version20241102164913.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241102164913 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE newsletter (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD departure_date DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE travel ADD period_start DATETIME DEFAULT NULL, ADD period_end DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE newsletter');
+        $this->addSql('ALTER TABLE reservation DROP departure_date');
+        $this->addSql('ALTER TABLE travel DROP period_start, DROP period_end');
+    }
+}
