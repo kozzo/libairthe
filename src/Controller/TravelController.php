5ec278f86261dfa89ada8af3f89b422c6be70492
kozzo <kevin.zochowski@gmail.com>
2024-11-03 13:54:08 +0100

    bug fix

--- src/Controller/TravelController.php
+++ src/Controller/TravelController.php
@@ -22,13 +22,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class TravelController extends AbstractController
 {
 
-	private $reservationService;
+    private $reservationService;
 
-	public function __construct(TravelSearchService $travelSearchService) {
-		$this->travelSearchService = $travelSearchService;
-	}
+    public function __construct(TravelSearchService $travelSearchService) {
+        $this->travelSearchService = $travelSearchService;
+    }
 
-	#[Route('/', name: 'app_travel')]
+    #[Route('/', name: 'app_travel')]
     public function index(TravelRepository $travelRepository): Response
     {
         $catalog = $travelRepository->findAll();
@@ -62,19 +62,18 @@ class TravelController extends AbstractController
     }
 
     #[Route('/{slug}', name: 'app_travel_page', methods: ['GET'])]
-    public function show(TravelRepository $travelRepository, string $slug): Response
+    public function show(Travel $travel): Response
     {
-        $travel = $travelRepository->findOneBy(['slug' => $slug]);
-
-        if (!$travel) {
-            throw $this->createNotFoundException('Travel page not found');
-        }
-
         return $this->render('travel/show.html.twig', [
             'travel' => $travel,
         ]);
     }
 
+    #[Route('/{slug}/edit', name: 'app_travel_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Travel $travel, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TravelType::class, $travel);
+        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
@@ -88,7 +87,7 @@ class TravelController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_travel_delete', methods: ['POST'])]
+    #[Route('/{slug}', name: 'app_travel_delete', methods: ['POST'])]
     public function delete(Request $request, Travel $travel, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$travel->getId(), $request->getPayload()->getString('_token'))) {
@@ -99,28 +98,28 @@ class TravelController extends AbstractController
         return $this->redirectToRoute('app_travel_index', [], Response::HTTP_SEE_OTHER);
     }
 
-	/**
-	 * @throws \Exception
-	 */
-	#[Route('/travels/search', name: 'travel_search')]
-	public function search(Request $request): Response
-	{
-		// Récupérer les critères de recherche depuis la requête
-		$criteria = [
-			'keyword'   => $request->query->get('keyword'),
-			'category'  => $request->query->get('category'),
-			'date'      => $request->query->get('date') ? new \DateTime($request->query->get('date')) : null,
-			'minRating' => $request->query->get('minRating') ? (int) $request->query->get('minRating') : null,
-			'maxBudget' => $request->query->get('maxBudget') ? (float) $request->query->get('maxBudget') : null,
-			'adults'    => $request->query->get('adults') ? (int) $request->query->get('adults') : 0,
-			'children'  => $request->query->get('children') ? (int) $request->query->get('children') : 0,
-		];
+    /**
+     * @throws \Exception
+     */
+    #[Route('/travels/search', name: 'travel_search')]
+    public function search(Request $request): Response
+    {
+        // Récupérer les critères de recherche depuis la requête
+        $criteria = [
+            'keyword'   => $request->query->get('keyword'),
+            'category'  => $request->query->get('category'),
+            'date'      => $request->query->get('date') ? new \DateTime($request->query->get('date')) : null,
+            'minRating' => $request->query->get('minRating') ? (int) $request->query->get('minRating') : null,
+            'maxBudget' => $request->query->get('maxBudget') ? (float) $request->query->get('maxBudget') : null,
+            'adults'    => $request->query->get('adults') ? (int) $request->query->get('adults') : 0,
+            'children'  => $request->query->get('children') ? (int) $request->query->get('children') : 0,
+        ];
 
-		$travels = $this->travelSearchService->searchTravels($criteria);
+        $travels = $this->travelSearchService->searchTravels($criteria);
 
-		return $this->render('travel/search_results.html.twig', [
-			'travels' => $travels,
-			'criteria' => $criteria,
-		]);
-	}
+        return $this->render('travel/search_results.html.twig', [
+            'travels' => $travels,
+            'criteria' => $criteria,
+        ]);
+    }
 }
